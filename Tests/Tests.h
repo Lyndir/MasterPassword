e967affddba5ebde139a0af1d6dc245e2529ee86
Maarten Billemont <lhunath@lyndir.com>
2012-07-04 23:57:29 +0200

    Added unit tests for playing around with the logic.

---
+++ Tests/Tests.h
@@ -0,0 +1,13 @@
+//
+//  Tests.h
+//  Tests
+//
+//  Created by Maarten Billemont on 04/07/12.
+//  Copyright (c) 2012 Lyndir. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Tests : SenTestCase
+
+@end
---
+++ Tests/Tests.m
@@ -0,0 +1,45 @@
+//
+//  Tests.m
+//  Tests
+//
+//  Created by Maarten Billemont on 04/07/12.
+//  Copyright (c) 2012 Lyndir. All rights reserved.
+//
+
+#import "Tests.h"
+
+@implementation Tests
+
+- (void)setUp
+{
+    dbg(@"======================= TEST SET-UP ======================");
+    [PearlLogger get].autoprintLevel = PearlLogLevelTrace;
+
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    dbg(@"===================== TEST TEAR-DOWN =====================");
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testAlgorithm
+{
+    NSString *masterPassword = @"test-mp";
+    NSString *username = @"test-user";
+    NSString *siteName = @"test-site";
+    MPElementType siteType = MPElementTypeGeneratedLong;
+    uint32_t siteCounter = 42;
+    
+    NSString *sitePassword = MPCalculateContent( siteType, siteName, keyForPassword( masterPassword, username ), siteCounter );
+    
+    inf( @"master password: %@, username: %@\nsite name: %@, site type: %@, site counter: %d\n    => site password: %@",
+               masterPassword, username, siteName, NSStringFromMPElementType(siteType), siteCounter, sitePassword );
+}
+
+@end
