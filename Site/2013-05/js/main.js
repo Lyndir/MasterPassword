cf8ecb29521ccc070f64d1d8b1ca8d51884bf7d6
Maarten Billemont <lhunath@lyndir.com>
2014-08-28 20:10:22 -0400

    Site style tweaks and Android beta link.

--- Site/2013-05/js/main.js
+++ Site/2013-05/js/main.js
@@ -1,4 +1,8 @@
 $(function(){
     $.stellar();
+
+    window.onscroll = function() {
+        document.getElementById("scrollDown").style.opacity = Math.max(0, 200 - window.scrollY) / 200;
+    };
 });
 
