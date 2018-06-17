b6e6dce9f04194e7581ba1144dd9ba7b238cf1a8
Maarten Billemont <lhunath@lyndir.com>
2018-06-17 01:48:36 -0400

    Added some missing build files.

--- platform-independent/c/core/src/mpw-jni.c
+++ platform-independent/c/core/src/mpw-jni.c
@@ -8,7 +8,7 @@
 
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
     JNIEnv* env;
-    if (vm->GetEnv(&env, JNI_VERSION_1_6) != JNI_OK)
+    if ((*vm)->GetEnv( vm, (void **)&env, JNI_VERSION_1_6 ) != JNI_OK)
         return -1;
 
     return JNI_VERSION_1_6;
